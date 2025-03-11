66ca5f864483afaf10507f0961dddb6cb6170a91
AcidOP <zeeshanalivr46@gmail.com>
2025-03-11 23:02:35 +0530

    feat: setup tailwind and fonts

---
+++ src/components/fonts.ts
@@ -0,0 +1,6 @@
+import { Bricolage_Grotesque } from 'next/font/google';
+
+export const fontBrico = Bricolage_Grotesque({
+  subsets: ['latin'],
+  variable: '--font-brico',
+});
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div className='container bg-red-700'>Navbar</div>;
+};
+
+export default Navbar;
